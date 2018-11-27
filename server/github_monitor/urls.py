4ccae9355991f27ee22a76994ee8aa44f47135cc
王松_Tuuu <song@secbox.cn>
2018-11-27 15:57:59 +0800

    :sparkles: server add authorization

--- server/github_monitor/urls.py
+++ server/github_monitor/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'api/monitor/', include('github_monitor.apps.monitor.urls'), name='monitor'),
+    path('login', views.obtain_auth_token),
 ]
